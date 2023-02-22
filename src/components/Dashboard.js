61d56f64ec3a43ec55ebc02c3bfd71d76f014324
ACF <alricfernandes11@gmail.com>
2023-02-22 13:16:44 -0500

    Binding this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -38,9 +38,9 @@ class Dashboard extends Component {
 
   // Method
   selectPanel(id) {
-    this.setState({
-      focused: id
-    });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
 
   // Render
@@ -57,10 +57,9 @@ class Dashboard extends Component {
       .map(panel => (
         <Panel
           key={panel.id}
-          id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
